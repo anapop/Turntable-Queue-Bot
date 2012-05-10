cf5d6d5741dfabfcca01a22588612aba7ce38bfd
Ana Pop <apop@tux.cs.princeton.edu>
2012-05-09 23:47:39 -0400

    simplified welcome message

--- tt_queue.js
+++ tt_queue.js
@@ -150,7 +150,7 @@ function is_mod(user_id){
 }
 
 function queue_instructions(){
-	var input_message = "÷÷÷÷÷÷÷÷÷÷÷Commands÷÷÷÷÷÷÷÷÷÷÷ [] w? [] w+ [] w- [] w++ [] -plays [] -mods []";
+	var input_message = "÷÷÷÷÷÷÷÷÷÷÷Commands÷÷÷÷÷÷÷÷÷÷÷ | w? | w+ | w- | w++ | -plays | -mods |";
 	deliver_chat(input_message);
 }
 function deliver_chat(input_message){
