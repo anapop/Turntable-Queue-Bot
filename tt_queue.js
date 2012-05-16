7fd66e8702b5d2915317e3783f6fa01529f354bd
Ana Pop <apop@opus.cs.princeton.edu>
2012-05-16 18:48:50 -0400

    added @ back

--- tt_queue.js
+++ tt_queue.js
@@ -258,7 +258,7 @@ function get_user_id(username, strict){
 }
 
 function get_user_name(user_id, strict){
-````if (strict == true){
+	if (strict == true){
 		if(user_hash[user_id]["name"].substring(0, 1) == '@') {
 			return user_hash[user_id]["name"];		
 		} else {
